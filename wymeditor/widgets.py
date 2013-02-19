43520dfd9861c4e2d211fcfc2f791c2d055edd7b
Benoit Domingue <bedingue@gmail.com>
2013-02-19 16:46:49 -0500

    add an option to prevent jquery from being loaded.
    this can be usefull if another widget is already loading jquery

--- wymeditor/widgets.py
+++ wymeditor/widgets.py
@@ -4,24 +4,31 @@ from django.contrib.admin import widgets
 
 
 class WYMEditorArea(forms.Textarea):
-    def __init__(self, attrs={}):
+    def __init__(self, attrs={}, load_jquery=True):
         base_class = attrs.get("class", "")
         attrs['class'] = " ".join((base_class, "WYMEditor",))
+        self.load_jquery = load_jquery
         super(WYMEditorArea, self).__init__(attrs=attrs)
 
-    class Media:
+    def _media(self):
+        js = [
+            '%swymeditor/jquery.wymeditor.js' % settings.STATIC_URL,
+            '%sjs/load_wymeditor.js' % settings.STATIC_URL,
+        ]
+        
+        if self.load_jquery:
+            js.insert(0, 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js')
+            
         css = {
             "all": (
-                "//ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/smoothness/jquery-ui.css",
+                "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/smoothness/jquery-ui.css",
                 '%swymeditor/skins/twopanels/skin.css' % settings.STATIC_URL,
             )
         }
-        js = (
-            '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
-            '%swymeditor/jquery.wymeditor.js' % settings.STATIC_URL,
-            '%sjs/load_wymeditor.js' % settings.STATIC_URL,
-        )
+
+        return forms.Media(js=js, css=css)
+    media = property(_media)
 
 
-class AdminWYMEditorArea(widgets.AdminTextareaWidget, WYMEditorArea):
+class AdminWYMEditorArea(WYMEditorArea, widgets.AdminTextareaWidget):
     pass
